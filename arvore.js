c6b4e55c4a0421eb17ce13f2aed97585a7a24a05
derek-alves <derek.alves@fatec.sp.gov.br>
2020-04-03 23:52:08 -0300

    Binary Search Tree: Insert + search

--- arvore.js
+++ arvore.js
@@ -1,4 +1,4 @@
-
+const readlineSync = require('readline-sync');
 //       +
 //      / \ 
 //     /   \
@@ -13,7 +13,6 @@
 //        /     \
 //     'c'      'd'
 //      
-const readlineSync = require('readline-sync');
 
 class Node{
     constructor(data){
@@ -90,31 +89,116 @@ class Tree{
     } 
 }
 
+class arvoreBinariaBusca extends Tree{
+    constructor(data = null){
+        super(data);
+        if(data){
+            var node = new Node(data);
+            this.root = node;
+        }else{
+            this.root = null;
+        }
+    }
+
+     insert(value){
+         var parent = null;
+         var x = this.root;
+         while(x){
+             parent = x;
+             if(value < x.data){
+                  x = x.left;  
+             }else{
+                 x = x.right;
+             }
+         }
+         if(parent == null){
+             this.root = new Node(value);
+         }else if (value < parent.data){
+             parent.left = new Node(value);
+         }else{
+             parent.right = new Node(value);
+         }
+
+    }
+
+    InOrder(node=null){
+        if (node == null){
+            node = this.root;
+            console.log('\nIn order:');
+        }
+        if (node.left){
+            this.InOrder(node.left);
+        }
+        process.stdout.write(`${node.data},`);
+        if (node.right){
+            this.InOrder(node.right);
+        }
+        
+    }
+
+    search(value){
+        return this._search(value,this.root);
+    }
+
+    _search(value,node){
+        if(node == null){
+            return node;
+        }
+        if (node.data == value) {
+            return new arvoreBinariaBusca(node);
+        }
+        if(value < node.data){
+            return this._search(value,node.left);
+        }
+        return this._search(value,node.right);
+    }
+}
+
+
 function Start(){
-    tree = new Tree();
-    n1 = new Node('a');
-    n2 = new Node('+');
-    n3 = new Node('*');
-    n4 = new Node('b');
-    n5 = new Node('-');
-    n6 = new Node('/');
-    n7 = new Node('c');
-    n8 = new Node('d');
-    n9 = new Node('e');
+    //tree = new Tree();
+    //n1 = new Node('a');
+    //n2 = new Node('+');
+    //n3 = new Node('*');
+    //n4 = new Node('b');
+    //n5 = new Node('-');
+   // n6 = new Node('/');
+    //n7 = new Node('c');
+    //n8 = new Node('d');
+    //n9 = new Node('e');
 
-    n6.left = n7;
-    n6.right = n8;
-    n5.left = n6;
-    n5.right = n9;
-    n3.left = n4;
-    n3.right = n5;
-    n2.left =  n1;
-    n2.right =  n3;
-    tree.root = n2;
+    //n6.left = n7;
+   // n6.right = n8;
+    //n5.left = n6;
+    //n5.right = n9;
+    //n3.left = n4;
+    //n3.right = n5;
+   // n2.left =  n1;
+   // n2.right =  n3;
+    //tree.root = n2;
 
-    tree.inOrder();
-    tree.postOrder();
-    console.log("\nAltura:",tree.height());
-}   
+    //tree.inOrder();
+    //tree.postOrder();
+    //console.log("\nAltura:",tree.height());
+
+    bst = new arvoreBinariaBusca();
+    var values = [10,40,2,30,90,82,200,110,14,24,23,55,320,41,78,73]
+    for (var v of values){
+    bst.insert(v);
+}
+bst.InOrder();
+   
+console.log('\n');
+var itens = [10,666,200,99]
+for (item of itens){
+    r = bst.search(item);
+    if (r == null){
+        console.log(item, 'não encontrado');
+    }else{
+        console.log(r.root.data.data, "encontrado!")
+    }
+}
+}
 
 Start();
+
